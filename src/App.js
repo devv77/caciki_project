c29b836bb33f76985dc0befb109c2ea58256b891
devv77 <lencsesdavid@gmail.com>
2022-03-29 20:52:32 +0200

    added a lot of things and mainly worked with css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import  { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './components/header/header.component'
+
 import './App.css';
+import HomePage from './pages/homepage/homepage.page';
+import Footer from './components/footer/footer.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
 }
